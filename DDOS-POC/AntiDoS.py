17cc482194e433116423c8ee47b6bf1bb5673e69
GIl Yelkin <gil.yelkin@gmail.com>
2024-08-12 15:16:20 +0300

    Created AntiDoS.py

---
+++ DDOS-POC/AntiDoS.py
@@ -0,0 +1,48 @@
+from scapy.all import sniff, get_if_addr, conf
+from scapy.layers.inet import IP
+from collections import deque
+import time
+from sys import stdout
+
+MY_IP = get_if_addr(conf.iface)
+MAX_HISTORY = 5
+
+
+def dos_detector(interface):
+    packets = deque(maxlen=MAX_HISTORY)  # Fixed-size queue for efficient packet handling
+    threshold = 3
+
+    def packet_handler(packet):
+        nonlocal packets, threshold
+        if IP not in packet:
+            # print(f"Skipped {packet}")
+            return
+
+        src_ip = packet[IP].src
+        if src_ip == MY_IP:
+            return
+
+        print(packet)
+        packets.append((time.time(), src_ip))
+        print(packets)
+
+        # Count packets from the same source in the last n seconds
+        packet_count = sum(1 for ts, ip in packets if ip == src_ip and ts >
+                           time.time() - 30)
+        if packet_count > threshold:
+            print(f"Potential DoS attack from {src_ip}")
+
+    sniff(iface=interface, prn=packet_handler)
+
+
+def main():
+    print(f"Started sniffing on host {MY_IP}...\n")
+    try:
+        dos_detector(conf.iface)
+    except KeyboardInterrupt:
+        print("\nSniffing stopped.")
+
+
+if __name__ == "__main__":
+    print("running")
+    main()
