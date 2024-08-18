5946dbdcabab7b551d0f0e9a3943d9246ba562f6
Mattan <73639004+mattanelkaim@users.noreply.github.com>
2024-08-18 18:51:08 +0300

    organized code

--- AntiARPSpoofing.py
+++ AntiARPSpoofing.py
@@ -6,41 +6,40 @@ from scapy.all import conf
 
 class ARPTableEntry:
     def __init__(self, ip_address: str, mac_address: str, entry_type: str, interface: Interceptor = conf.iface):
-        """
-        Initialize an ARP table entry.
-
-        :param ip_address: IP address associated with the entry (str).
-        :param mac_address: MAC address associated with the entry (str).
-        :param interface: Network interface associated with the entry (str).
-        """
         self.ip: str = ip_address
         self.mac: str = mac_address
         self.type: str = entry_type
         self.interface: Interceptor = interface
 
-    def __str__(self):
-        """
-        Return a string representation of the ARP table entry.
-        """
-        return f"IP: {self.ip}, MAC: {self.mac}, Type: {self.type}, Interface: {self.interface}"
+    def __str__(self) -> str:
+        # Nice padding for organized printing
+        return f"IP: {self.ip:<15} | MAC: {self.mac} | Type: {self.type:<7} | Interface: {self.interface}"
 
 
 def detect_ARP_spoofing() -> None:
     pass
 
 
-def get_ARP_table() -> list[ARPTableEntry]:
+def get_ARP_table() -> list[ARPTableEntry] | None:
     try:
         table: list[ARPTableEntry] = []
         output = subprocess.run(['arp', '-a'], capture_output=True, text=True, check=True).stdout
-        interface = conf.ifaces.dev_from_index(int(output.splitlines()[1].split(' ')[-1], 16))
-        for line in output.splitlines()[3::]:
+
+        # Take the last value in interface row, which is the interface ID in hexa
+        interface = conf.ifaces.dev_from_index(int(output.splitlines()[1].split(' ')[-1], 16))  # Convert from hexa to decimal
+
+        for line in output.splitlines()[3:]:  # Skip the first 3 lines
             arp_entry_match = re.compile(r'^\s*(\d+\.\d+\.\d+\.\d+)\s+([0-9a-fA-F\-]+)\s+(\w+)\s*$').match(line)
             if arp_entry_match:
-                x = ARPTableEntry(arp_entry_match.group(1), arp_entry_match.group(2), arp_entry_match.group(3), interface)
-                table.append(x)
-
+                #                   Internet Address (IP)    Physical Address (MAC)    Type (static/dynamic)
+                entry = ARPTableEntry(arp_entry_match.group(1), arp_entry_match.group(2), arp_entry_match.group(3), interface)
+                table.append(entry)
         return table
+
     except subprocess.CalledProcessError as e:
         print(f"Failed to retrieve ARP table. Error: {e}")
         return None
+
+
+for i in get_ARP_table():
+    print(i)
