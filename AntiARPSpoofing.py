ad8e7593d8cafc4df9ed309d94f63f0d51016bb7
Mattan <73639004+mattanelkaim@users.noreply.github.com>
2024-08-20 02:56:14 +0300

    almost finished

--- AntiARPSpoofing.py
+++ AntiARPSpoofing.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 import subprocess
 import re
 from dataclasses import dataclass
+from idlelib.autocomplete import TRY_A
+
 from scapy.config import Interceptor
 from scapy.all import sniff, conf
 from scapy.layers.l2 import ARP
@@ -19,21 +21,43 @@ class ARPTableEntry:
         # Nice padding for organized printing
         return f"IP: {self.ip:<15} | MAC: {self.mac} | Type: {self.type:<7} | Interface: {self.interface}"
 
+@dataclass(frozen=True, slots=True)
+class ARPRequest:
+    requested_ip: str
+    sender_ip: str
+
+    # Change eq method for convenience
+    def __eq__(self, other_sender_ip: str) -> bool:
+        return self.sender_ip == other_sender_ip
+
+@dataclass(frozen=True, slots=True)
+class ARPResponse:
+    responder_ip: str
+    response_mac: str
 
 def detect_ARP_spoofing(interface: Interceptor) -> None:
     table: list[ARPTableEntry] = get_ARP_table()
+    [print(x) for x in table]
+    print()
+    requests: list[ARPRequest] = []
 
     def packet_handler(packet):
-        content = packet[ARP].summary()
+        content = packet[ARP].summary().split('/')[0]  # Scapy shit
         if packet.op == 1:  # Request
-            request = content.split('/')[0]
+            ips_match = re.compile(r"^.* (\d+\.\d+\.\d+\.\d+).* (\d+\.\d+\.\d+\.\d+)\s*$").match(content)
+            request = ARPRequest(*ips_match.groups())  # Unpack groups
+            requests.append(request)
             print(request)
-            ips_match = re.compile(r"^.* (\d+\.\d+\.\d+\.\d+).* (\d+\.\d+\.\d+\.\d+)\s*$").match(request)
-            print(f'Sender: {ips_match.group(2)} | Requested: {ips_match.group(1)}\n')
         elif packet.op == 2:  # Response
-            print(content)
             results = re.compile(r"^.* (([0-9A-Fa-f]{2}[:-]){5}[0-9A-Fa-f]{2}).* (\d+\.\d+\.\d+\.\d+)\s*$").match(content)
-            print(f'Result: {results.group(1)} | Responder: {results.group(3)}\n')  # 2nd group is trash
+            # Discard 2nd group because of regex syntax
+            response = ARPResponse(results.group(3), results.group(1))
+            request = requests.pop(requests.index(packet.pdst))  # find dest
+            print(f"{response} | {request}")
+            # Need to validate in that response.response_mac is
+            # same as the MAC of request.requested_ip in my table
+            print([x for x in table if x.ip == request.requested_ip])  # Doesn't work??
+
 
     sniff(iface=interface, prn=packet_handler, timeout=120, promisc=True, store=False, filter='arp')
     pass
