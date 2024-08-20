29091513a15705b305b43d9d37a88808e8bd3d1b
Gil_Yelkin <gil.yelkin@gmail.com>
2024-08-20 20:59:27 +0300

    fixed detect_ARP_spoofing

--- AntiARPSpoofing.py
+++ AntiARPSpoofing.py
@@ -5,12 +5,39 @@ import re
 from dataclasses import dataclass
 from idlelib.autocomplete import TRY_A
 
-from scapy.config import Interceptor
-from scapy.all import sniff, conf
+from scapy.interfaces import get_working_ifaces
+from scapy.config import Interceptor, conf
+from scapy.all import sniff, get_if_addr, get_if_hwaddr
 from scapy.layers.l2 import ARP
 
 
-@dataclass(slots=True)
+@dataclass
+class ARPTable:
+    entries: list[ARPTableEntry]
+
+    def __init__(self):
+        self.entries = get_ARP_table()
+        if self.entries is None:
+            raise Exception
+
+        # adding our own IP and MAC addresses to the table for ease of use
+        for interface in get_working_ifaces():
+            self.entries.append(ARPTableEntry(get_if_addr(interface), get_if_hwaddr(interface), 'dynamic', interface))
+
+    def get_mac(self, ip: str, interface: Interceptor) -> str | None:
+        for entry in self.entries:
+            if interface == entry.interface and ip == entry.ip:
+                return entry.mac
+        return None
+
+    def __str__(self):
+        s: str = ''
+        for entry in self.entries:
+            s += entry.__str__() + '\n'
+        return s
+
+
+@dataclass
 class ARPTableEntry:
     ip: str
     mac: str
@@ -19,9 +46,10 @@ class ARPTableEntry:
 
     def __str__(self) -> str:
         # Nice padding for organized printing
-        return f"IP: {self.ip:<15} | MAC: {self.mac} | Type: {self.type:<7} | Interface: {self.interface}"
+        return f"IP: {self.ip:<15} | MAC: {self.mac} | Type: {self.type:<7} | Interface: {self.interface.name}"
 
-@dataclass(frozen=True, slots=True)
+
+@dataclass(frozen=True)
 class ARPRequest:
     requested_ip: str
     sender_ip: str
@@ -30,19 +58,22 @@ class ARPRequest:
     def __eq__(self, other_sender_ip: str) -> bool:
         return self.sender_ip == other_sender_ip
 
-@dataclass(frozen=True, slots=True)
+
+@dataclass(frozen=True)
 class ARPResponse:
     responder_ip: str
     response_mac: str
 
-def detect_ARP_spoofing(interface: Interceptor) -> None:
-    table: list[ARPTableEntry] = get_ARP_table()
-    [print(x) for x in table]
-    print()
+
+def detect_ARP_spoofing(interface: Interceptor) -> list[str]:
+    table: ARPTable = ARPTable()
+    print(table)
+    attackers: list[str] = []
     requests: list[ARPRequest] = []
 
     def packet_handler(packet):
-        content = packet[ARP].summary().split('/')[0]  # Scapy shit
+        nonlocal table, interface
+        content = packet[ARP].summary().split('/')[0]  # ignoring the padding
         if packet.op == 1:  # Request
             ips_match = re.compile(r"^.* (\d+\.\d+\.\d+\.\d+).* (\d+\.\d+\.\d+\.\d+)\s*$").match(content)
             request = ARPRequest(*ips_match.groups())  # Unpack groups
@@ -56,11 +87,17 @@ def detect_ARP_spoofing(interface: Interceptor) -> None:
             print(f"{response} | {request}")
             # Need to validate in that response.response_mac is
             # same as the MAC of request.requested_ip in my table
-            print([x for x in table if x.ip == request.requested_ip])  # Doesn't work??
-
+            my_data = table.get_mac(request.requested_ip, interface)
+            print('Validating response...')
+            print(f'Your data: {my_data} | actual response: {response.response_mac}')
+            if my_data == response.response_mac:
+                print('Answers match, response is valid.')
+            else:
+                print(f'Answers mismatch, adding {response.responder_ip} to the threats list.')
+                attackers.append(response.responder_ip)
 
     sniff(iface=interface, prn=packet_handler, timeout=120, promisc=True, store=False, filter='arp')
-    pass
+    return attackers
 
 
 def get_ARP_table() -> list[ARPTableEntry] | None:
